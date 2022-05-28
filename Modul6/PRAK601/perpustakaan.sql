b3a7f6779d5bebdb82fde283e77c76d20aad64a2
Maulana <2010817310008@mhs.ulm.ac.id>
2022-05-28 16:17:08 +0800

    Update perpustakaan.sql

--- Modul6/PRAK601/perpustakaan.sql
+++ Modul6/PRAK601/perpustakaan.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 13, 2022 at 05:50 PM
+-- Generation Time: May 28, 2022 at 10:16 AM
 -- Server version: 10.4.22-MariaDB
 -- PHP Version: 8.1.2
 
@@ -40,7 +40,7 @@ CREATE TABLE `buku` (
 --
 
 INSERT INTO `buku` (`id_buku`, `judul_buku`, `penulis`, `penerbit`, `tahun_terbit`) VALUES
-(1, 'KKN Di Desa Penari', 'Simpleman', 'Bukune', 2019),
+(1, 'KKN Di Desa Penari', 'Simpleman', 'Bukune', 2022),
 (2, 'Sebuah Seni untuk Bersikap Bodo Amat', 'Mark Manson', 'Gramedia Widiasarana Indonesia', 2005),
 (3, 'Start With Why', 'Simon Sinek', 'Gramedia Pustaka Utama', 2019),
 (4, 'Maaf Tuhan, Aku Hampir Menyerah', 'ALFIALGHAZI', 'SAHIMA', 2020),
@@ -48,9 +48,7 @@ INSERT INTO `buku` (`id_buku`, `judul_buku`, `penulis`, `penerbit`, `tahun_terbi
 (6, 'Kesetiaan Mr. X (Devotion of Suspect X)', 'Keigo Higashino', ' Gramedia Pustaka Utama', 2021),
 (7, 'Home Sweet Loan', 'Almira Bastari', 'Gramedia Pustaka Utama', 2022),
 (8, 'Indigo Tapi Penakut', 'Angelyn Stevanie', 'Media Kita', 2022),
-(9, 'Ekspedisi Warung Kopi', 'PAI BIAN', 'LOVEABLE X ROMANCIOUS', 2021),
-(10, 'Diary Of Canva', 'ITAKRN', 'Kawah Media Pustaka', 2022),
-(11, 'Sejarah Dunia Yang Disembunyikan', 'Jonathan Black', 'Alvabeta', 2015);
+(9, 'Ekspedisi Warung Kopi', 'PAI BIAN', 'LOVEABLE X ROMANCIOUS', 2021);
 
 -- --------------------------------------------------------
 
@@ -62,6 +60,7 @@ CREATE TABLE `member` (
   `id_member` int(11) NOT NULL,
   `nama_member` varchar(250) NOT NULL,
   `nomor_member` varchar(15) NOT NULL,
+  `password` varchar(60) NOT NULL,
   `alamat` text NOT NULL,
   `tgl_mendaftar` datetime NOT NULL,
   `tgl_terakhir_bayar` date NOT NULL
@@ -71,10 +70,11 @@ CREATE TABLE `member` (
 -- Dumping data for table `member`
 --
 
-INSERT INTO `member` (`id_member`, `nama_member`, `nomor_member`, `alamat`, `tgl_mendaftar`, `tgl_terakhir_bayar`) VALUES
-(1, 'Maulana', '082256407791', '  Bati-Bati, Tanah Laut', '2022-05-10 19:14:32', '2022-05-13'),
-(2, 'May Er', '088812341234', '  Liang Anggang', '2022-05-10 19:16:17', '2022-05-17'),
-(3, 'Dr. Strange', '0866666666666', 'California, New York', '1970-01-01 01:00:00', '2022-05-13');
+INSERT INTO `member` (`id_member`, `nama_member`, `nomor_member`, `password`, `alamat`, `tgl_mendaftar`, `tgl_terakhir_bayar`) VALUES
+(1, 'Maulana', '082256407791', '1', '  Bati-Bati, Tanah Laut, Kalimantan Sealatan  ', '2022-05-05 01:00:00', '2022-05-13'),
+(2, 'May Er', '088812341234', 'c81e728d9d4c2f636f067f89cc14862c', '  Liang Anggang     ', '2022-05-10 19:16:00', '2022-05-17'),
+(3, 'Dr. Strange', '0866666666666', 'b53b3a3d6ab90ce0268229151c9bde11', 'California, New York  ', '1970-01-01 01:00:00', '2022-05-13'),
+(5, 'Admin', '1234', '1234', 'Admin', '2022-05-24 18:24:27', '2022-05-26');
 
 -- --------------------------------------------------------
 
@@ -95,7 +95,8 @@ CREATE TABLE `peminjaman` (
 INSERT INTO `peminjaman` (`id_peminjaman`, `tgl_pinjam`, `tgl_kembali`) VALUES
 (1, '2022-05-12', '2022-05-14'),
 (2, '2022-05-11', '2022-05-23'),
-(3, '2022-05-13', '2022-05-31');
+(3, '2022-05-13', '2022-05-31'),
+(5, '2022-05-25', '2022-05-27');
 
 --
 -- Indexes for dumped tables
@@ -111,7 +112,8 @@ ALTER TABLE `buku`
 -- Indexes for table `member`
 --
 ALTER TABLE `member`
-  ADD PRIMARY KEY (`id_member`);
+  ADD PRIMARY KEY (`id_member`),
+  ADD UNIQUE KEY `nomor_member` (`nomor_member`);
 
 --
 -- Indexes for table `peminjaman`
@@ -127,19 +129,19 @@ ALTER TABLE `peminjaman`
 -- AUTO_INCREMENT for table `buku`
 --
 ALTER TABLE `buku`
-  MODIFY `id_buku` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `id_buku` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
 -- AUTO_INCREMENT for table `member`
 --
 ALTER TABLE `member`
-  MODIFY `id_member` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id_member` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT for table `peminjaman`
 --
 ALTER TABLE `peminjaman`
-  MODIFY `id_peminjaman` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id_peminjaman` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
